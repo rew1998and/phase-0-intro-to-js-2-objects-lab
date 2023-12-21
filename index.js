e42d8960ae469dea0aaa256a59d83550e0e914a7
Andrew <rew1998and@gmail.com>
2023-12-20 21:33:29 -0800

    complete

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+let employee = {
+    name: 'John',
+    age: 30,
+    role: 'Software Engineer'
+};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+let updatedEmployee = Object.assign({}, employee);
+updatedEmployee[key]= value;
+return updatedEmployee;
+}
+let updatedEmployee = updateEmployeeWithKeyAndValue(employee, 'name', 'Ada');
+console.log(updatedEmployee);
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key,) {
+    let clone = Object.assign({}, employee);
+    delete clone[key];
+    return clone;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
